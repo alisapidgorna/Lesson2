0a3a43e763a4fe598409a63077411efd5a70e6fd
Alisa Pidgorna <a.pidgorna@ukr.net>
2020-02-29 22:40:02 +0200

    Initial commit

---
+++ src/main/java/Homework.java
@@ -0,0 +1,42 @@
+public class Homework {
+    public static void main (String[]args) {
+        System.out.println("Alisa");
+        int a=2;
+        int b=3;
+        int sum=1;
+        int dif=2;
+        float divide=3;
+        long multiply=4;
+        sum+=a;
+        dif-=a;
+        divide/=a;
+        multiply*=a;
+        System.out.println("Result with adding:"+sum);
+        System.out.println("Variable type dif:"+dif);
+        System.out.println("Variable type dif:"+divide);
+        System.out.println("Variable type dif:"+multiply);
+
+
+        /*int f=a+b;
+        long multipleResult=a*b;
+        int vychytanie=a-b;
+        double divide=a/b;
+        System.out.println("Переменная со сложения:"+ f);
+        System.out.println("-------------------------");
+        System.out.println("Результат умножения:"+ multipleResult);
+        System.out.println("-------------------------");
+        System.out.println("Результат вычитания:" + vychitanie);
+        System.out.println("-------------------------");
+        System.out.println("Результат деления:"+ double divide);
+        System.out.println("-------------------------");
+         */
+        int k=3;
+        int d=4;
+        ++k; // k=4
+        --d; // k=3
+        System.out.println("Incremental result:"+k);
+        System.out.println("Decremental result:"+d);
+
+
+    }
+}
